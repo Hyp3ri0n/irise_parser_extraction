8f9df65ee1d234890501e86bf4387012675d6d7c
Hyp3ri0n <aurelien2405@gmail.com>
2018-06-11 23:14:53 +0200

    FIX optimisation of the parser

--- parser.go
+++ parser.go
@@ -9,7 +9,22 @@ import (
 	"bufio"
 	"regexp"
 	"time"
+	"bytes"
 )
+
+type telemetry struct {
+    date string
+	heure string
+	state string
+	value string
+}
+
+type data struct {
+	homeId string
+	deviceId string
+	deviceName string
+    telem []telemetry
+}
 
 func Parser(src, target string) error {
 	files, err := ioutil.ReadDir(src)
@@ -69,111 +84,142 @@ func ParseFile(src, filename, target string) error {
 	pattern_home := regexp.MustCompile("^HOUSEHOLD\\s:\\s([0-9]*)");
 	pattern_line := regexp.MustCompile("^(([0-9]{2}\\/?){3})\\t(([0-9]{2}:?){2})\\t([0-9]*)\\t([0-9]*)");
 
-	output_device := "";
-	deviceId := strings.Replace(strings.Split(filename,"-")[2], ".txt", "", 1);
-	output_home := "";
-	homeId := "";
-	output_line := "";
+	data := data{deviceId: strings.Replace(strings.Split(filename,"-")[2], ".txt", "", 1)}
+
 	for scanner.Scan() {
 		line := scanner.Text();
 
 		results_device := pattern_device.FindStringSubmatch(line);
 		results_home := pattern_home.FindStringSubmatch(line);
 		results_line := pattern_line.FindStringSubmatch(line);
+
 		if len(results_device) > 0 {
-			deviceName := results_device[1];
-			output_device += deviceId + ";" + deviceName;
+			data.deviceName = results_device[1];
 		}
 		if len(results_home) > 0 {
-			homeId = results_home[1];
-			output_home += homeId;
+			data.homeId = results_home[1];
 		}
 		if len(results_line) > 0 {
-			date := results_line[1];
-			heure := results_line[3];
-			state := results_line[5];
-			value := results_line[6];
-			output_line += homeId + ";" + deviceId + ";" + strings.Replace(date, "/", "-", 3) + "T" + heure + ";" + state + ";" + value + "\n";
+			
+			telem := telemetry{
+				date: results_line[1],
+				heure: results_line[3],
+				state: results_line[5],
+				value: results_line[6]}
+			data.telem = append(data.telem, telem);
 		}
 	}
 
 	timeEnd := time.Now();
 
-	fmt.Print("PARSING FILE end (secondes not ms) : ");
-	fmt.Print((timeEnd.Sub(timeStart)/1000));
-	fmt.Print(" s\n");
+	fmt.Print("PARSING FILE end : ");
+	fmt.Println(timeEnd.Sub(timeStart));
 
-	if err := writeCSV(output_device, output_home, output_line, target, filename); err != nil {
+	if err := write_CSV_stratBasic(data, target+"CSV/strat_basic/", filename); err != nil {
 		fmt.Println("ERROR CREATE DIST : ");
 		fmt.Println(err);
         return err
-    }
+	}
+	
+	/*if err := write_CSV_stratOnUpdate(data, target+"CSV/strat_onUpdate/", filename); err != nil {
+		fmt.Println("ERROR CREATE DIST : ");
+		fmt.Println(err);
+        return err
+    }*/
 
 	return nil;
 }
 
-func writeCSV(device_data, home_data, line_data, target, filename string) error {
+func write_CSV_stratBasic(data data, target, filename string) error {
 	
+	// We change the extension of the target (txt to CSV)
 	filename = strings.Replace(filename, ".txt", ".csv", 1);
 
+	// create data
+	timeStart := time.Now();
+
+	device_data := data.deviceId + ";" + data.deviceName;
+	home_data := data.homeId;
+
+	var line_data bytes.Buffer;
+	for i := range data.telem {
+		line_data.WriteString(data.homeId);
+		line_data.WriteString(";");
+		line_data.WriteString(data.deviceId);
+		line_data.WriteString(";");
+		line_data.WriteString(strings.Replace(data.telem[i].date, "/", "-", 3));
+		line_data.WriteString("T");
+		line_data.WriteString(data.telem[i].heure);
+		line_data.WriteString(";");
+		line_data.WriteString(data.telem[i].state);
+		line_data.WriteString(";");
+		line_data.WriteString(data.telem[i].value);
+
+		if i < len(data.telem) {
+			line_data.WriteString("\n");
+		}
+	}
+
+	timeEnd := time.Now();
+
+	fmt.Print("CREATE BUFFER end : ");
+	fmt.Println(timeEnd.Sub(timeStart));
+
+	// We create the folder target
     if err := os.MkdirAll(target, 0755); err != nil {
 		fmt.Println("ERROR CREATE DIR CSV : ");
 		fmt.Println(err);
         return err
     }
 
-	err := ioutil.WriteFile(target + filename, []byte(line_data), 0755);
-	if err != nil {
-		fmt.Println("ERROR CREATE FILE CSV : ");
+	// We write the CSV fike that contains telemetry's data
+	if err := appendFile(target + filename, line_data.String()); err != nil {
+		fmt.Println("ERROR WRITE FILE CSV : ");
 		fmt.Println(err);
 		return err;
 	}
 
-	fmt.Print("LINE WRITE : ");
-	fmt.Print(len(line_data));
-	fmt.Println(" bytes");
-
-	f, err := os.OpenFile(target + "appliance.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755);
-	if err != nil {
-		fmt.Println("ERROR CREATE FILE APPLIANCE CSV : ");
+	// We write the CSV fike that contains telemetry's data
+	if err := appendFile(target + "all_telemetry.csv", line_data.String()); err != nil {
+		fmt.Println("ERROR WRITE FILE ALL_TELEMETRY CSV : ");
 		fmt.Println(err);
 		return err;
 	}
 
-	device_data += "\n";
-	nb, err := f.WriteString(device_data);
-	if err != nil {
+	// We write the appliance CSV
+	if err := appendFile(target + "appliance.csv", device_data); err != nil {
 		fmt.Println("ERROR WRITE FILE APPLIANCE CSV : ");
 		fmt.Println(err);
 		return err;
 	}
 
-	fmt.Print("APPLIANCE WRITE : ");
-	fmt.Print(nb);
-	fmt.Println(" bytes");
-
-	
-	fh, err := os.OpenFile(target + "household.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755);
-	if err != nil {
-		fmt.Println("ERROR CREATE FILE APPLIANCE CSV : ");
+	// We write the household CSV
+	if err := appendFile(target+"household.csv", home_data); err != nil {
+		fmt.Println("ERROR WRITE FILE HOUSEHOLD CSV : ");
 		fmt.Println(err);
 		return err;
 	}
 
-	home_data += "\n";
-	nbh, err := fh.WriteString(home_data);
+	return nil;
+}
+
+func appendFile(target, content string) error {
+	f, err := os.OpenFile(target, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755);
 	if err != nil {
-		fmt.Println("ERROR WRITE FILE APPLIANCE CSV : ");
-		fmt.Println(err);
 		return err;
 	}
 
-	fmt.Print("HOUSEHOLD WRITE : ");
+	nbh, err := f.WriteString(content);
+	if err != nil {
+		return err;
+	}
+
+	fmt.Print("APPEND WRITE ON " + target + " : ");
 	fmt.Print(nbh);
 	fmt.Println(" bytes");
 
 
-	fh.Close();
+	f.Close();
 
 	return nil;
 }
@@ -191,9 +237,8 @@ func main() {
 	timeStart := time.Now();
 	Parser("output/unziped/", "output/parsed/");
 	timeEnd := time.Now();
-	fmt.Print("PARSING end (secondes not ms) : ");
-	fmt.Print((timeEnd.Sub(timeStart)/1000));
-	fmt.Print(" s\n");
+	fmt.Print("PARSING end : ");
+	fmt.Println(timeEnd.Sub(timeStart));
 	fmt.Println("PARSING files : DONE");
 
 	fmt.Println("TODO : CLEAN ZIPED FILES");
